a49ec3dac065eb975603a4d36b8ba844613ff3ec
Tomi <tomi_emmen@hotmail.com>
2018-09-09 15:20:46 +0000

    http to https rewrites

--- public/scripts/visuals/show.js
+++ public/scripts/visuals/show.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
     //add fluid classes to correctly display images
     $('img').addClass('img-fluid col-12 no-padding-margin');
 
-    $.each($('img'), function(index, image){
-        var imageUrl = $(image).attr('src');
-        imageUrl = imageUrl.substring(4);
-        imageUrl = 'https' + imageUrl;
-        $(image).attr('src', imageUrl);
-    });
+    // $.each($('img'), function(index, image){
+    //     var imageUrl = $(image).attr('src');
+    //     imageUrl = imageUrl.substring(4);
+    //     imageUrl = 'https' + imageUrl;
+    //     $(image).attr('src', imageUrl);
+    // });
 
 
     $('img').attr('src').replace('http', 'https');
