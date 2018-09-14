24f370c0eff7f51e1c767bb44fcc8aaaf60f88fc
UnrealSpecter <tomi_emmen@hotmail.com>
2018-09-14 11:23:24 +0000

    analytics for the website added

--- resources/views/scripts.blade.php
+++ resources/views/scripts.blade.php
@@ -5,5 +5,15 @@
     <script src="{{ URL::asset('plugins/hammer/js/hammer.min.js') }}"></script>
     <script src="{{ URL::asset('plugins/hammer/js/jquery-hammer.js') }}"></script>
     <script src="{{ URL::asset('plugins/bootstrap-4.0/js/bootstrap.min.js')}}"></script>
-    
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125791936-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-125791936-1');
+    </script>
+
     @yield('additional-scripts')
