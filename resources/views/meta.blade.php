b8254f3843b6a2395eca18e6b576d9a2a15971a7
Tomi <tomi_emmen@hotmail.com>
2018-09-25 09:36:15 +0000

    google site verfication

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
 
     <meta name="description" content="project-code-base">
+    <meta name="google-site-verification" content="othxOFLQWF60UhP24bFcryyuPS2Of_eQ4YHRcIKnoHI" />
 
     <link rel="icon" type="image/png" href="{{ asset('/assets/sigar-content/boys-r-toys.png') }}">
 
