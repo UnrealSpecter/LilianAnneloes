496c6d90385b66b15745b39b3530d1ffd35c921d
vagrant <vagrant@homestead>
2018-11-01 13:52:23 +0000

    web update

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::get('/textual', function () {
 Route::get('/textual/notebook', 'NotebookController@index');
 Route::get('/about', 'AboutController@index');
 Route::get('/about/cv', 'CVController@index');
-Route::get('/about/news', 'EventController@index');
-Route::get('/about/news/{id}', 'EventController@show');
+Route::get('/about/news', 'Frontend\EventController@index');
+Route::get('/about/news/{id}', 'Frontend\EventController@show');
 
 // visuals and writings index (overviews)
 Route::get('/visuals', 'Frontend\VisualController@index');
