1329438dc67575a4cf367a1a993b9ecdf0eb23fb
UnrealSpecter <tomi_emmen@hotmail.com>
2021-03-02 10:50:25 +0100

    updated meta tags

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -2,8 +2,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
 
-    <meta name="description" content="Lilian Anneloes (03-03-1997) is a Dutch visual and textual artist.
-Her work, mostly consisting of film, performance, text and drawing, revolves around topics like seksuality, sex, gender, genderroles, womanhood and personal life experiences. The works take an intersectional feminist/equalist approach and explore social-political issues around named topics. ">
+    <meta name="title" content="LILIAN ANNELOES">
+    <meta name="description" content="Lilian Anneloes (1997) is a Dutch feminist activist visual artist and writer.  
+
+Her work, mostly consisting of drawing, film and writing, revolves around intersectional feminist topics such as gender, sex, sexuality, or observations of privilege, but related personal experiences and (institutional) critique also play a part within all three media she employs.
+
+Lilian graduated Fine Art at the Minerva Art Academy in Groningen in 2018 and is currently working on receiving her masters degree in Art History (Modern and Contemporary art) at the Rijksuniversiteit Groningen.">
+    <meta name="author" content="LILIAN ANNELOES">
+    <meta name="keywords" content="visual, textual, artist, performance, seksuality, sex, gender, genderroles, womanhood, personal, life, experiences">
 
     <meta name="google-site-verification" content="othxOFLQWF60UhP24bFcryyuPS2Of_eQ4YHRcIKnoHI" />
 
